5c5c9735fa8fbfc489174de9fccdcfb4e4c3d2e1
Scott Dupoy <scott@dupoy.com>
2014-10-11 22:25:09 +0100

    got access token retrievals working; session caching, login, logout. http 443 is not the same as https you chump! :)

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var favicon = require('serve-favicon');
 var yaml = require('js-yaml');
 var fs = require('fs');
@@ -18,6 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 //app.use(express.logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: config.security.sessionSecret,
@@ -28,6 +30,7 @@ app.use(session({
 // routes
 app.get('/', routes.home(config));
 app.get('/authorized', routes.authorized(config));
+app.get('/logout', routes.logout());
 
 server.listen(config.http.port, config.http.host, function() {
   console.log('server listening on ' + config.http.host + ':' + config.http.port);
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,27 @@
-var http = require('http');
+var https = require('https');
 
 exports.home = function(config) {
   return function(req, res) {
-    // TODO: check for token in session, if !present => home, otherwise => landing
-    // landing needs a logout button which will clear the session
-    res.render('home', { clientId: config.strava.clientId });
+    if (req.session == null || req.session.access_token == null) {
+      console.log('home');
+      return res.render('home', { clientId: config.strava.clientId });
+    }
+    console.log('landing: ' + req.session.athlete_firstname + ' ' + req.session.athlete_lastname);
+    return res.render('landing', {
+      access_token: req.session.access_token,
+      id: req.session.athlete_id,
+      firstname: req.session.athlete_firstname,
+      lastname: req.session.athlete_lastname,
+      email: req.session.athlete_email
+    });
+  };
+};
+
+exports.logout = function() {
+  return function(req, res) {
+    console.log('logging out');
+    req.session.destroy();
+    res.redirect('/');
   };
 };
 
@@ -14,27 +31,38 @@ exports.authorized = function(config) {
       console.log('Login not authorized');
       return res.redirect('/');
     }
-    res.redirect('/');
-    console.log('Login authorized:');
-    console.log('  code:  ' + req.query.code);
     
     var path = '/oauth/token?client_id=' + config.strava.clientId + '&client_secret=' + config.strava.clientSecret + '&code=' + req.query.code;
-    var accessTokenRequest = http.request({
+    var accessTokenRequest = https.request({
       host: 'www.strava.com',
-      port: 80,
+      port: 443,
       path: path,
       method: 'POST'
     },
     function(tokenResponse) {
-      console.log('token response headers: ' + JSON.stringify(tokenResponse.headers));
-      // TODO: get access token and put it in the session!
-    })    
+      var data = '';
+      tokenResponse.on('data', function(chunk) {
+        data += chunk;
+      });
+      tokenResponse.on('end', function() {
+        data = JSON.parse(data);
+        req.session.access_token = data.access_token;
+        req.session.athlete_id = data.athlete.id;
+        req.session.athlete_firstname = data.athlete.firstname;
+        req.session.athlete_lastname = data.athlete.lastname;
+        req.session.athlete_profile_medium = data.athlete.profile_medium;
+        req.session.athlete_profile_large = data.athlete.profile;
+        req.session.athlete_data_preference = data.athlete.data_preference;
+        req.session.athlete_measurement_preference = data.athlete.measurement_preference;
+        req.session.athlete_email = data.athlete.email;
+        res.redirect('/');
+      });
+    })
     .on('error', function(err) {
-      // TODO: redirect to an error page
        console.log('get access token error: ' + err);
+       res.redirect('/');
     })
     .end();
-
   };
 };
 
